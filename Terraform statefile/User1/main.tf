80d17d538cbe6e640cf41ad7ab161d69ae8286a7
A Manish <54245851+Manish9010@users.noreply.github.com>
2023-02-20 22:58:19 +0530

    Terraform statefile locking

---
+++ Terraform statefile/User1/main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+    region                   = "us-east-1"
+    shared_credentials_file = "~/.aws/credentials"
+    profile                  = "Terraform1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "my-aws-bucket-storage-tfs"
+    key = "path/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "locking-table-helper"
+    shared_credentials_file = "~/.aws/credentials"
+    profile                  = "Terraform1"
+  }
+}
+
+
+resource "aws_instance" "myinstance" {
+    ami = "ami-002070d43b0a4f171"
+    instance_type = "t2.micro"
+    count=1
+}
+
